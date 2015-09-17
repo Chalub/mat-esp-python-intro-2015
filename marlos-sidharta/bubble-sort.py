8b7ae98d55b4cbee89ae6ac30c224ede07ac0bb5
marlos <marlosreis@hotmail.com>
2015-09-17 13:03:42 -0300

    aidiconei os comentarios no arquivo bubble-sort.py

--- marlos-sidharta/bubble-sort.py
+++ marlos-sidharta/bubble-sort.py
@@ -1,8 +1,16 @@
-N=20
-lista=[11,18,3,1,16,12,6,19,5,0,14,4,17,9,13,7,10,15,2,8]
-for i in range(0,N-1,1):
-    for j in range(i+1,N-1,1):
+#total de algarismos
+N=20 
+#algarismos da variavel lista
+lista=[11, 18, 3, 1, 16, 12, 6, 19, 5, 0, 14, 4, 17, 9, 13, 7, 10, 15, 2, 8]
+#de quanto em quanto sera organizada a lista
+for i in range(0, N - 1, 1):
+# comando para gerar a lista
+    for j in range(i + 1, N , 1):
+# comando para trocar os elementos da lista toda vez que i for maior que j 
         if lista[i]>lista[j]:
+# cria uma variavel que guarda os elementos da lista i, temporariamente, enquanto estao ocorrendo as trocas 
             temp=lista[i]
+# agora igualamos a variavel i com o elemento da variavel j
             lista[i]=lista[j]
+# por fiim, podemos associar o elementoda lista j a variavel temp, pois trocamos de lugar a variavel i e a variavel j
             lista[j]=temp
