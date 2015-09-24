d7fb399514569304c97bac0df7635c3cc7b58bbb
sidharta <sidharta_soares@hotmail.com>
2015-09-24 14:42:11 -0300

    criei o comando para criar os graficos com as trocas

--- marlos-sidharta/bubble-sort.py
+++ marlos-sidharta/bubble-sort.py
@@ -1,7 +1,18 @@
+import matplotlib.pyplot as plt
+a=0
 lista=[11, 18, 3, 1, 16, 12, 6, 19, 5, 0, 14, 4, 17, 9, 13, 7, 10, 15, 2, 8]
 print("Lista original:", lista)
 #total de algarismos
 N=20 
+x=range(0,N)
+y=lista
+plt.figure()
+plt.title("Lista original")
+plt.xlabel("Indices")
+plt.ylabel("Numeros")
+plt.plot(x, y, 'ok')
+plt.savefig("Fig/bubble-inicio.png")
+plt.close()
 #algarismos da variavel lista
 lista=[11, 18, 3, 1, 16, 12, 6, 19, 5, 0, 14, 4, 17, 9, 13, 7, 10, 15, 2, 8]
 #de quanto em quanto sera organizada a lista
@@ -16,6 +27,25 @@ for i in range(0, N - 1, 1):
             lista[i]=lista[j]
 # por fiim, podemos associar o elementoda lista j a variavel temp, pois trocamos de lugar a variavel i e a variavel j
             lista[j]=temp
+            a=a+1
+            x=range(0,N)
+            y=lista
+            plt.figure()
+            plt.title("Lista troca")
+            plt.xlabel("Indices")
+            plt.ylabel("Numeros")
+            plt.plot(x, y, "ok")
+            plt.savefig("Fig/bubble-troca-{}".format(a))
+            plt.close()
 print ("Lista em ordem crescente:", lista)
 print ("Cinco maiores valores:", lista[N-5:N])
 print ("Cinco menores valores:", lista[0:N-15])
+x=range(0,N)
+y=lista
+plt.figure()
+plt.title("Lista em ordem crescente")
+plt.xlabel("Indices")
+plt.ylabel("Numeros")
+plt.plot(x, y, 'ok')
+plt.savefig("Fig/bubble-fim.png")
+plt.close
